a2834f746311022a2fefe6ba764557d2a1b10ac5
FABJAV-UTN <41366311@alumnos.frsr.utn.edu.ar>
2026-05-03 20:20:23 -0300

    se corrigió settings para cumplir con 12-Factor App, KISS

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,8 +1,16 @@
 import os
 
 class Settings:
-    # Estas variables coinciden con lo que busca mongo_connection.py
+    # MongoDB
     MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
     DB_NAME = os.getenv("DB_NAME", "pdf_extraction_db")
+    
+    # NUEVO: Configuración de archivos (12-Factor App: config en env vars)
+    MAX_PDF_SIZE_MB = int(os.getenv("MAX_PDF_SIZE_MB", "10"))
+    MAX_PDF_SIZE_BYTES = MAX_PDF_SIZE_MB * 1024 * 1024
+    
+    # NUEVO: Configuración de paginación (para Paso 4)
+    DEFAULT_PAGE_SIZE = int(os.getenv("DEFAULT_PAGE_SIZE", "20"))
+    MAX_PAGE_SIZE = int(os.getenv("MAX_PAGE_SIZE", "100"))
 
 settings = Settings()
